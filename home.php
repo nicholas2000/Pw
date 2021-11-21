8cc932a61aed4aa711d75187325cfca270040a95
nicholashwe <sughapur@gmail.com>
2021-11-21 11:39:18 +0700

    Soto

--- home.php
+++ home.php
@@ -8,6 +8,6 @@
     <link rel="stylesheet" href="uni.css">
 </head>
 <body>
-    
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate temporibus consequuntur id quam quisquam quo doloremque perspiciatis? Nemo eaque, similique, magnam enim eum animi nulla asperiores qui in itaque doloribus!
 </body>
 </html>
